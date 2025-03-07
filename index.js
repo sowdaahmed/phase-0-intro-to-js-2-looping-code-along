d43bb42b00b4063d67c8849811468757ab59aa65
Sowda <ayanmoha4949@gmail.com>
2025-03-07 05:43:49 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Function to create thank-you messages
+function writeCards(names, occasion) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to zero
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions so tests can find them
+  module.exports = { writeCards, countDown };
+  
